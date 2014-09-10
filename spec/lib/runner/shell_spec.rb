7c76d711d65c8bd6da2e2222f5ebd4826156c780
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-09-10 12:55:45 -0500

    Shell tests

--- spec/lib/runner/shell_spec.rb
+++ spec/lib/runner/shell_spec.rb
@@ -4,6 +4,12 @@ describe Codescout::Runner::Shell do
   let(:shell) { described_class.new }
 
   describe "#execute" do
-    pending
+    it "returns true on success" do
+      expect(shell.execute("date")).to eq true
+    end
+
+    it "returns false on failure" do
+      expect(shell.execute("mkdir /tmp")).to eq false
+    end
   end
 end
